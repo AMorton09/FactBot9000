fba4d32e3cafb1ecf513d493a772b9691c0b87c5
Andrew Morton <morton09@me.com>
2017-08-03 11:25:09 -0500

    running

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-/**
- * Created by 1337 on 8/3/2017.
- */
+const options = require("./options.json");
+const fact = require("./facts/fact.json")
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+
+
+
+
+
+bot.on("ready", () => {
+    console.log("I am ready!");
+});
+
+bot.on("message", (message) => {
+    if (message.content.startsWith(options.prefix+"test")) {
+        message.channel.send("i Work!");
+
+}
+
+    if (message.content.startsWith(options.prefix+"fact")) {
+
+        message.channel.send(fact.one);
+    }
+});
+
+
+
+
+
+bot.login(options.token);
