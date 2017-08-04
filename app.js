31816bd81a48ae24bc5224a97cd23eae93fb61a7
Andrew Morton <morton09@me.com>
2017-08-03 19:10:16 -0500

    added ascArt

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ bot.on("message",  (message) => {
         message.channel.send("i Work!");
 
 }
+    //FLUSH CHAT
     if (message.content.startsWith(options.prefix+"flush")){
         for ( i=0;i<25;i++ ){
             if (i%5 == 0 ) wait(500);
@@ -37,17 +38,40 @@ bot.on("message",  (message) => {
 
         }
     }
+    //RANDOM FFACTS
     if (message.content.startsWith(options.prefix+"fact")) {
 
         message.channel.send(randomProperty(fact));
     }
-
+    //HARRYPOTTER
     if (message.content.startsWith(options.prefix+"1911")) {
 
-        message.channel.send(copyPasta.spam);
+        message.channel.send(copyPasta.arry1);
+        message.channel.send(copyPasta.arry2);
+        message.channel.send(copyPasta.arry3);
+        message.channel.send(copyPasta.arry4);
+        message.channel.send(copyPasta.arry5);
     }
-
-
+    //SIXTY SECONDS IN AFRICA
+    if (message.content.startsWith(options.prefix+"afrika")){
+        message.channel.send(copyPasta.africa)
+    }
+    //DONGER RAISER
+    if (message.content.startsWith(options.prefix+"donger")){
+        message.channel.send(copyPasta.donger)
+    }
+    //doritos
+    if (message.content.startsWith(options.prefix+"doritoz")){
+        message.channel.send(copyPasta.doritos)
+    }
+    //vibe
+    if (message.content.startsWith(options.prefix+"vibe")){
+        message.channel.send(copyPasta.vibe)
+    }
+    //virus
+    if (message.content.startsWith(options.prefix+"wyrus")){
+        message.channel.send(copyPasta.virus)
+    }
 });
 
 
