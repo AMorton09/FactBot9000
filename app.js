764d316faa6b22558bd657965ead3cc50993690f
Andrew Morton <morton09@me.com>
2017-11-30 14:18:28 -0600

    fixed kappa spam

--- app.js
+++ app.js
@@ -8,69 +8,67 @@ const bot = new Discord.Client();
 //method for grabbing a random property
 let randomProperty = function (obj) {
     let keys = Object.keys(obj);
-    return obj[keys[ keys.length * Math.random() << 0]];
+    return obj[keys[keys.length * Math.random() << 0]];
 };
 
 
-let purgeMessages = (message) =>{
+let purgeMessages = (message) => {
     let count = 0;
-    message.channel.fetchMessages({limit: 100}).then(messages =>{
+    message.channel.fetchMessages({limit: 100}).then(messages => {
         let msgArr = messages.array();
         let numMsg = msgArr.length;
 
-        for(let i = 0; i < numMsg; i++) {
+        for (let i = 0; i < numMsg; i++) {
             msgArr[i].delete()
-                .then(function() {
+                .then(function () {
                     count++;
-                    if(count >= 100) {
+                    if (count >= 100) {
                         purgeMessages();
                     }
                 })
-                .catch(function() {
+                .catch(function () {
                     count++;
-                    if(count >= 100) {
+                    if (count >= 100) {
                         purgeMessages();
                     }
                 })
         }
     })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log('SOMETHING WENT HORRIBLY WRONG!!!!!!!');
             console.log(err);
         });
 };
 
 
-
-
 bot.on("ready", () => {
     console.log("I am ready!");
 });
 
-bot.on("message",  (message) => {
+bot.on("message", (message) => {
     if (!message.content.startsWith(options.prefix)) return;
-    if (message.content.startsWith(options.prefix+"test")) {
+    if (message.content.startsWith(options.prefix + "test")) {
         message.channel.send("i Work!");
     }
 
     //Purge messages for last 14 days
-    if (message.content.startsWith(options.prefix+"purgeRecent")){
-        message.channel.bulkDelete(100,true);
+    if (message.content.startsWith(options.prefix + "purgeRecent")) {
+        message.channel.bulkDelete(100, true);
     }
 
     //Complete message purge
-    if (message.content.startsWith(options.prefix+"purge")){
+    if (message.content.startsWith(options.prefix + "purge")) {
         purgeMessages(message);
     }
 
 
     //RANDOM FFACTS
-    if (message.content.startsWith(options.prefix+"fact")) {
+    if (message.content.startsWith(options.prefix + "fact")) {
 
         message.channel.send(randomProperty(fact));
     }
     //HARRYPOTTER
-    if (message.content.startsWith(options.prefix+"1911")) {
+    if (message.content.startsWith(options.prefix + "1911")) {
 
         message.channel.send(copyPasta.arry1);
         message.channel.send(copyPasta.arry2);
@@ -80,27 +78,27 @@ bot.on("message",  (message) => {
     }
 
     //DONGER RAISER
-    if (message.content.startsWith(options.prefix+"donger")){
+    if (message.content.startsWith(options.prefix + "donger")) {
         message.channel.send(copyPasta.donger)
     }
     //doritos
-    if (message.content.startsWith(options.prefix+"doritoz")){
+    if (message.content.startsWith(options.prefix + "doritoz")) {
         message.channel.send(copyPasta.doritos)
     }
     //vibe
-    if (message.content.startsWith(options.prefix+"vibe")){
+    if (message.content.startsWith(options.prefix + "vibe")) {
         message.channel.send(copyPasta.vibe)
     }
     //virus
-    if (message.content.startsWith(options.prefix+"wyrus")){
+    if (message.content.startsWith(options.prefix + "wyrus")) {
         message.channel.send(copyPasta.virus)
     }
     //wronghood
-    if (message.content.startsWith(options.prefix+"hood")){
+    if (message.content.startsWith(options.prefix + "hood")) {
         message.channel.send(copyPasta.wrongHood)
     }
     //riot
-    if (message.content.startsWith(options.prefix+"riot")){
+    if (message.content.startsWith(options.prefix + "riot")) {
         message.channel.send(copyPasta.riot);
         message.channel.send(copyPasta.riot);
         message.channel.send(copyPasta.riot);
@@ -109,7 +107,7 @@ bot.on("message",  (message) => {
 
     }
     //spam kappa
-    if (message.content.startsWith(options.prefix+"kappaSpam123")){
+    if (message.content.startsWith(options.prefix + "kappaSpam123")) {
         message.channel.send(copyPasta.kappa);
         message.channel.send(copyPasta.kappa);
         message.channel.send(copyPasta.kappa);
@@ -118,7 +116,7 @@ bot.on("message",  (message) => {
 
     }
     //sad panda
-    if (message.content.startsWith(options.prefix+"sadPanda")){
+    if (message.content.startsWith(options.prefix + "sadPanda")) {
         message.channel.send(copyPasta.sadPanda);
         message.channel.send(copyPasta.sadPanda);
         message.channel.send(copyPasta.sadPanda);
@@ -127,9 +125,6 @@ bot.on("message",  (message) => {
     }
 
 });
-
-
-
 
 
 bot.login(options.token);
